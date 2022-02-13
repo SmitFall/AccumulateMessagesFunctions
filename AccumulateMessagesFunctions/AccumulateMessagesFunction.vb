b288227b525c02b143a1dc0d6755546d484146a5
Smitfall <captainfalconrsmith@gmail.com>
2022-02-12 18:05:22 -0700

    Will add link

--- AccumulateMessagesFunctions/AccumulateMessagesFunction.vb
+++ AccumulateMessagesFunctions/AccumulateMessagesFunction.vb
@@ -2,7 +2,7 @@
 'RCET0265
 'Spring 2022
 'Accumlate Messages Function
-'
+'https://github.com/SmitFall/AccumulateMessagesFunctions/tree/master/AccumulateMessagesFunctions
 
 
 Option Explicit On
@@ -20,18 +20,20 @@ Module AccumulateMessagesFunction
         End Try
 
         Do
-
+            'promt the user to type anything
             Console.WriteLine("please type a message:")
             userInput = Console.ReadLine()
             UserMessages(userInput, False)
             Console.WriteLine(UserMessages("", False))
+            'user types a message and it displays it
         Loop While userInput <> "Q"
         Console.Read()
+
     End Sub
 
     Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
         Static messages As String
-
+        'Takes the users message 
         If clear = True Then
             messages = ""
         ElseIf newMessage <> "" Then
